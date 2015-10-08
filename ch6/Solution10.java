0fac73d30d249873a035eb24a390702f392c7fb9
Tejashree Gharat <tgharat@andrew.cmu.edu>
2015-10-08 11:49:44 -0400

    Update Solution10.java

--- ch6/Solution10.java
+++ ch6/Solution10.java
@@ -9,7 +9,7 @@ import java.util.*;
  *
  * @author Tejashree pc
  */
-//referred to book solution
+
 /*
 We can do this by approaching the test as a binary indicator of positive or negative test for poisoned bottles of soda.
 Since there are 1000 keys and only 10 test strips, we need to figure out a way to map the 1000bottles to 10strips at a time.
@@ -19,7 +19,8 @@ We can thus carry out the test for 2^10 = 1024 bottles of soda.
 
 After waiting for 7days we read the results,there will be strips i with a positive test for poison. Enter 1 in the ith bit positions and determine the id of the bottle by looking at the test results of all test strips.
 */
-public class Poison {
+//referred to book solution
+public class Solution10 {
     public static void main(String args[]) throws IOException
     {
     
